3bd533b04d244a87f414e204e80c86bbb6b5aa84
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-06-09 22:39:28 -0300

    Add get and put books routes

--- src/infra/router/books-routes.ts
+++ src/infra/router/books-routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express"
 import { BooksController } from "../../app/controller/books-controller"
 import routerAdapter from "./router-adapter"
+import { BooksRepositoryMongoose } from "../repository/books-repository"
+import { BooksUseCase } from "../../app/useCases/books-useCases"
 
 const BooksRoutes = (router: Router) => {
-  const booksController = new BooksController()
+  const booksUseCase = new BooksUseCase(new BooksRepositoryMongoose())
+  const booksController = new BooksController(booksUseCase)
 
   router.post("/books", routerAdapter(booksController, "create"))
+  router.get("/books", routerAdapter(booksController, "find"))
+  router.put("/books/:id", routerAdapter(booksController, "update"))
 }
 
 export { BooksRoutes }
