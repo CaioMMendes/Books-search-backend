b3ed3a10ba06fb9da1f6b46a46fc2f5dcc97f8ee
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-06-09 22:38:42 -0300

    feat: Add find and update functions to books-controller

--- src/app/controller/books-controller.ts
+++ src/app/controller/books-controller.ts
@@ -1,16 +1,13 @@
 import { HttpRequest, HttpResponse } from "../../infra/http/http-adapter"
 import { BookDto, bookSchema } from "../dto/books-dto"
+import { BooksUseCase } from "../useCases/books-useCases"
 
 class BooksController {
-  constructor() {}
+  constructor(private readonly booksUseCase: BooksUseCase) {}
 
-  show() {}
-
-  find() {}
-
-  async create(HttpRequest: HttpRequest): Promise<HttpResponse> {
+  async create(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const books: BookDto = bookSchema.parse(HttpRequest.body)
+      const books: BookDto = bookSchema.parse(httpRequest.body)
 
       if (!books) {
         return {
@@ -19,14 +16,71 @@ class BooksController {
         }
       }
 
+      const response = await this.booksUseCase.createBook(books)
+
       return {
         status: 201,
         message: "Livro criado com sucesso",
+        data: response,
       }
     } catch (error) {
       return {
         status: 400,
         message: "Ocorreu um erro ao tentar criar o livro",
+        error: error,
+      }
+    }
+  }
+
+  async find(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const dto: BookDto = httpRequest.query
+
+      if (!dto) {
+        return {
+          status: 400,
+          message: "Missing query",
+        }
+      }
+
+      const response = await this.booksUseCase.findBook(dto)
+
+      return {
+        status: 200,
+        message: "Livro encontrado com sucesso",
+        data: response,
+      }
+    } catch (error) {
+      return {
+        status: 400,
+        message: "Ocorreu um erro ao tentar encontrar o livro",
+        error: error,
+      }
+    }
+  }
+  async update(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const dto: BookDto = httpRequest.body
+      const id: string = httpRequest.params.id
+
+      if (!dto) {
+        return {
+          status: 400,
+          message: "Missing query",
+        }
+      }
+
+      const response = await this.booksUseCase.updateBook(dto, id)
+
+      return {
+        status: 200,
+        message: "Livro encontrado com sucesso",
+        data: response,
+      }
+    } catch (error) {
+      return {
+        status: 400,
+        message: "Ocorreu um erro ao tentar encontrar o livro",
         error: error,
       }
     }
